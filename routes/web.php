a09ccefa324bfa09daa7ba5c5d0f1f7320f4be30
maryamrana1234 <maryam.mshodja2yahoo.com>
2021-08-04 08:48:22 +0430

    web templates added to the project

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('client.home');});
+Route::get('/adminpanel', function () {return view('admin.home');});
